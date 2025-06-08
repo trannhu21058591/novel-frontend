b8e468297f0ece0009a888a92a836be52b265adf
Nhu <quynhnhu18121812@gmail.com>
2025-06-08 23:27:45 +0700

    update

--- src/utils/numberUtils.ts
+++ src/utils/numberUtils.ts
@@ -14,7 +14,7 @@ export const formatNumber = (num: number): string => {
         return (num / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'M';
     }
     if (num >= 1_000) {
-        return (num / 1_000).toFixed(1).replace(/\.0$/, '') + 'k';
+        return (num / 1_000).toFixed(1).replace(/\.0$/, '') + 'K';
     }
     return num.toString();
 };
