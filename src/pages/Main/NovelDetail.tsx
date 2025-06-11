f1ef4faad2cb46a8322586ed5de4a35f9782a262
Nhu <quynhnhu18121812@gmail.com>
2025-06-11 22:06:50 +0700

    update UI

--- src/pages/Main/NovelDetail.tsx
+++ src/pages/Main/NovelDetail.tsx
@@ -82,7 +82,7 @@ const NovelDetail = () => {
       </div>
 
       {/* Phần thêm dưới nếu có */}
-      <div className="w-full max-w-6xl mx-auto my-6 px-14 grid grid-cols-3 gap-5">
+      <div className="w-full max-w-6xl mx-auto my-6 px-14 grid grid-cols-3 gap-10">
         <div className="col-span-2">
             {/* Author Info */}
             <div className="flex flex-row items-center gap-3 mb-4">
@@ -156,7 +156,106 @@ const NovelDetail = () => {
                 </div>
             </div>
         </div>
-        <div className="col-span-1">2</div>
+        <div className="col-span-1 space-y-5">
+          {/*Author's Related Novels */}
+          <div className="bg-white rounded-lg border border-gray-200 shadow-sm p-4">
+            <h3 className="text-xl font-bold text-gray-900 mb-4">Các tác phẩm khác của Tác giả</h3>
+            <div className="space-y-2">
+              <div className="hover:cursor-pointer flex items-center justify-between hover:bg-gray-100 transition-colors duration-150">
+                <span className="text-gray-900 my-2 px-1.5">Tác phẩm 1</span>
+              </div>
+              <div className="hover:cursor-pointer flex items-center justify-between hover:bg-gray-100 transition-colors duration-150">
+                <span className="text-gray-900 my-2 px-1.5">Tác phẩm 2</span>
+              </div>
+              <div className="hover:cursor-pointer flex items-center justify-between hover:bg-gray-100 transition-colors duration-150">
+                <span className="text-gray-900 my-2 px-1.5">Tác phẩm 3</span>
+              </div>
+            </div>
+            <div className="mt-4 w-full flex justify-center">
+              <button className="hover:cursor-pointer w-full px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition-colors duration-150">
+                Xem thêm
+              </button>
+            </div>
+          </div>
+
+          {/* Các tác phẩm bạn có thể thích */}
+          <div className="bg-white rounded-lg border border-gray-200 shadow-sm p-4">
+            <h3 className="text-xl font-bold text-gray-900 mb-4">Bạn có thể thích</h3>
+            <div className="space-y-4">
+              {/* Novel Item 1 */}
+              <div className="hover:cursor-pointer w-full flex gap-3 hover:bg-gray-100 p-2 rounded-lg transition-colors duration-150">
+                <div className="w-[90px] h-[140px] rounded-lg overflow-hidden border border-gray-200 flex-shrink-0">
+                  <img
+                    src="https://i.pinimg.com/736x/46/db/bb/46dbbbbcdfc877ec7dfeb64cfad7a2ba.jpg"
+                    alt="Novel Cover"
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+                <div className="flex-1 min-w-0">
+                  <p className="mb-1 text-[16px] font-bold text-gray-900 line-clamp-1 break-words">
+                    [Đang Ra] Thiên Tài Y Học
+                  </p>
+                 
+                  <div className="flex items-center gap-1 mb-2">
+                    <List className="w-4 h-4 text-gray-500"/>
+                    <span className="text-[12px] text-gray-500">156 Chương</span>
+                  </div>
+                  <p className="text-[13px] text-gray-600 line-clamp-4">
+                    Hành trình của một thiên tài y học trẻ tuổi, đối mặt với những thách thức và bí ẩn trong ngành y. Trên con đường tìm các phương pháp y học đã gặp được người đi cùng mình đến suốt đời.
+                  </p>
+                </div>
+              </div>
+
+              {/* Novel Item 2 */}
+              <div className="hover:cursor-pointer w-full flex gap-3 hover:bg-gray-100 p-2 rounded-lg transition-colors duration-150">
+                <div className="w-[90px] h-[140px] rounded-lg overflow-hidden border border-gray-200 flex-shrink-0">
+                  <img
+                    src="https://i.pinimg.com/736x/46/db/bb/46dbbbbcdfc877ec7dfeb64cfad7a2ba.jpg"
+                    alt="Novel Cover"
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+                <div className="flex-1 min-w-0">
+                  <p className="mb-1 text-[16px] font-bold text-gray-900 line-clamp-1 break-words">
+                    [Đang Ra] Thiên Tài Y Học
+                  </p>
+                 
+                  <div className="flex items-center gap-1 mb-2">
+                    <List className="w-4 h-4 text-gray-500"/>
+                    <span className="text-[12px] text-gray-500">156 Chương</span>
+                  </div>
+                  <p className="text-[13px] text-gray-600 line-clamp-4">
+                    Hành trình của một thiên tài y học trẻ tuổi, đối mặt với những thách thức và bí ẩn trong ngành y. Trên con đường tìm các phương pháp y học đã gặp được người đi cùng mình đến suốt đời.
+                  </p>
+                </div>
+              </div>
+
+              {/* Novel Item 3 */}
+              <div className="hover:cursor-pointer w-full flex gap-3 hover:bg-gray-100 p-2 rounded-lg transition-colors duration-150">
+                <div className="w-[90px] h-[140px] rounded-lg overflow-hidden border border-gray-200 flex-shrink-0">
+                  <img
+                    src="https://i.pinimg.com/736x/46/db/bb/46dbbbbcdfc877ec7dfeb64cfad7a2ba.jpg"
+                    alt="Novel Cover"
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+                <div className="flex-1 min-w-0">
+                  <p className="mb-1 text-[16px] font-bold text-gray-900 line-clamp-1 break-words">
+                    [Đang Ra] Thiên Tài Y Học
+                  </p>
+                 
+                  <div className="flex items-center gap-1 mb-2">
+                    <List className="w-4 h-4 text-gray-500"/>
+                    <span className="text-[12px] text-gray-500">156 Chương</span>
+                  </div>
+                  <p className="text-[13px] text-gray-600 line-clamp-4">
+                    Hành trình của một thiên tài y học trẻ tuổi, đối mặt với những thách thức và bí ẩn trong ngành y. Trên con đường tìm các phương pháp y học đã gặp được người đi cùng mình đến suốt đời.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
